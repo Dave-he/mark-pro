44dd753995a0415a632b77fe795c54a1e459b43c
heyongxian <heyongxian@kuaishou.com>
2025-05-27 20:16:47 +0800

    1

--- run.sh
+++ run.sh
@@ -2,26 +2,26 @@
 cd src 
 
 #训练 UNet++
-python3 main.py --config configs/unetpp.yaml
+python3 main.py --model default
 if [ $? -eq 0 ]; then
-    python3 main.py --config configs/unetpp.yaml
+    python3 main.py --model default --predict true
 else
     echo "训练失败，未执行预测。"
 fi
 
 
 #训练 rmvl
-python3 main.py --config configs/rmvl.yaml
+python3 main.py --model mask
 if [ $? -eq 0 ]; then
-    python3 main.py --config configs/rmvl.yaml
+    python3 main.py --model mask --predict true
 else
     echo "训练失败，未执行预测。"
 fi
 
 #训练 mask-rcnn
-python3 main.py --config configs/mask-rcnn.yaml
+python3 main.py --model rmvl
 if [ $? -eq 0 ]; then
-    python3 main.py --config configs/mask-rcnn.yaml
+    python3 main.py --model rmvl --predict true
 else
     echo "训练失败，未执行预测。"
 fi
