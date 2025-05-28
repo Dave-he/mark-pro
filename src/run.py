7c28fafd9485e20731df40809d41d21f07f67956
heyongxian <heyongxian@kuaishou.com>
2025-05-28 20:48:03 +0800

    commit

--- src/run.py
+++ src/run.py
@@ -13,15 +13,15 @@ def main():
     parser = argparse.ArgumentParser(description="选择训练脚本")
     parser.add_argument('--mode', type=str, default='default', choices=['default', 'mask', 'rmvl'], help='选择训练模式')
     args = parser.parse_args()
-    if args.predict:
-        if args.mode == 'default':
-            predict.batch_predict()
-        elif args.mode =='mask':
-            predict_mask.main()
-        elif args.mode == 'rmvl':
-            predict_rmvl.main()
-        else:
-            print('未知模式，请选择 default、mask 或 rmvl')
+
+    if args.mode == 'default':
+        predict.batch_predict()
+    elif args.mode =='mask':
+        predict_mask.main()
+    elif args.mode == 'rmvl':
+        predict_rmvl.main()
+    else:
+        print('未知模式，请选择 default、mask 或 rmvl')
 
 if __name__ == '__main__':
     main()
