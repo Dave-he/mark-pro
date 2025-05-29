3fd2f23a1aacd1cc31619ae679916515681e709c
heyongxian <heyongxian@kuaishou.com>
2025-05-29 17:16:59 +0800

    run

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 cd src 
 
+#训练 UNet++ mask模式
+python3 main.py --mode mask
+if [ $? -eq 0 ]; then
+    python3 run.py --mode mask
+else
+    echo "训练失败，未执行预测。"
+fi
+
+
 #训练 UNet++
 python3 main.py --mode default
 if [ $? -eq 0 ]; then
@@ -10,15 +19,6 @@ else
 fi
 
 
-#训练 rmvl
-python3 main.py --mode mask
-if [ $? -eq 0 ]; then
-    python3 run.py --mode mask
-else
-    echo "训练失败，未执行预测。"
-fi
-
-#训练 mask-rcnn
 python3 main.py --mode rmvl
 if [ $? -eq 0 ]; then
     python3 run.py --mode rmvl
